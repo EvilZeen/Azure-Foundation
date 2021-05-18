958b1e1c4c2abbabfea21ecfc90cac1d39f4da28
Wildan Rizqullah <71007152+EvilZeen@users.noreply.github.com>
2021-05-18 10:58:52 +0800

    First Commit

---
+++ faq.js
@@ -0,0 +1,33 @@
+/*
+<!--University of Kuala Lumpur-->
+<!--WILDAN RIZQULLAH BIN KHAIRUL NIZAM 52052120061-->
+<!--SAYED AMIRUL HAZIQ BIN SAYED MOHAMAD KAMIL 52052120077-->
+<!--Azure Foundation-->
+<!--NORSHAHARIZAN PUTEH-->
+<!--WEEK 17-->
+*/
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+    
+    // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
+    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    // }
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+    
+  });
+});
